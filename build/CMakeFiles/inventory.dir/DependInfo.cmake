e8daa8bb830576888a6e6d43be3d0505ca27cb70
Franciszek <franciszek.wasiak08@gmail.com>
2026-04-15 22:25:05 +0200

    added the servant and master concept

---
+++ build/CMakeFiles/inventory.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/franciszekwasiak/CLionProjects/inventory_proj/Shop.cpp" "CMakeFiles/inventory.dir/Shop.cpp.o" "gcc" "CMakeFiles/inventory.dir/Shop.cpp.o.d"
+  "/Users/franciszekwasiak/CLionProjects/inventory_proj/game.cpp" "CMakeFiles/inventory.dir/game.cpp.o" "gcc" "CMakeFiles/inventory.dir/game.cpp.o.d"
+  "/Users/franciszekwasiak/CLionProjects/inventory_proj/inventory.cpp" "CMakeFiles/inventory.dir/inventory.cpp.o" "gcc" "CMakeFiles/inventory.dir/inventory.cpp.o.d"
+  "/Users/franciszekwasiak/CLionProjects/inventory_proj/item.cpp" "CMakeFiles/inventory.dir/item.cpp.o" "gcc" "CMakeFiles/inventory.dir/item.cpp.o.d"
+  "/Users/franciszekwasiak/CLionProjects/inventory_proj/itemGenerator.cpp" "CMakeFiles/inventory.dir/itemGenerator.cpp.o" "gcc" "CMakeFiles/inventory.dir/itemGenerator.cpp.o.d"
+  "/Users/franciszekwasiak/CLionProjects/inventory_proj/main.cpp" "CMakeFiles/inventory.dir/main.cpp.o" "gcc" "CMakeFiles/inventory.dir/main.cpp.o.d"
+  "/Users/franciszekwasiak/CLionProjects/inventory_proj/player.cpp" "CMakeFiles/inventory.dir/player.cpp.o" "gcc" "CMakeFiles/inventory.dir/player.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/inventory.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inventory.dir/Shop.cpp.o"
+  "CMakeFiles/inventory.dir/Shop.cpp.o.d"
+  "CMakeFiles/inventory.dir/game.cpp.o"
+  "CMakeFiles/inventory.dir/game.cpp.o.d"
+  "CMakeFiles/inventory.dir/inventory.cpp.o"
+  "CMakeFiles/inventory.dir/inventory.cpp.o.d"
+  "CMakeFiles/inventory.dir/item.cpp.o"
+  "CMakeFiles/inventory.dir/item.cpp.o.d"
+  "CMakeFiles/inventory.dir/itemGenerator.cpp.o"
+  "CMakeFiles/inventory.dir/itemGenerator.cpp.o.d"
+  "CMakeFiles/inventory.dir/main.cpp.o"
+  "CMakeFiles/inventory.dir/main.cpp.o.d"
+  "CMakeFiles/inventory.dir/player.cpp.o"
+  "CMakeFiles/inventory.dir/player.cpp.o.d"
+  "inventory"
+  "inventory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inventory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
